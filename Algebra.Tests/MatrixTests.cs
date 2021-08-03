511217f634e56c98ac9ee265d5cdd830af921896
josepo <jlpolo@gmail.com>
2021-08-03 17:02:34 +0200

    Solution and project files

---
+++ Algebra.Tests/MatrixTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Algebra.Tests
+{
+    public class MatrixTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Algebra/Matrix.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Algebra
+{
+    public class Matrix
+    {
+    }
+}
